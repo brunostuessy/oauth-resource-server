5d0f16140deb290d3533dd6aff82660d3f279a76
brunostuessy <bruno.stuessi@gmail.com>
2024-11-21 11:53:42 +0100

    Update cors config

--- src/main/java/com/baeldung/resource/config/WebSecurityConfig.java
+++ src/main/java/com/baeldung/resource/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.baeldung.resource.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -12,8 +14,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
@@ -23,8 +23,6 @@ public class WebSecurityConfig {
         http.cors(c -> c.configurationSource(corsConfigurationSource()))
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
-                //.requestMatchers("/error/**").permitAll();
-                //.requestMatchers("/api/auth/**").permitAll();
                 .requestMatchers("/user/info", "/api/foos/**").hasAuthority("SCOPE_read")
                 .requestMatchers("/api/foos").hasAuthority("SCOPE_write")
                 .anyRequest().authenticated()
@@ -41,21 +39,20 @@ public class WebSecurityConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        WebCorsConfigProperties cors = webCorsConfigProperties();
+        final WebCorsConfigProperties cors = webCorsConfigProperties();
 
-        CorsConfiguration configuration = new CorsConfiguration();
+        final CorsConfiguration configuration = new CorsConfiguration();
 
-        //configuration.setAllowedOriginPatterns(Arrays.asList(cors.getAllowedOrigins()));
+        //configuration.setAllowedOriginPatterns(Arrays.asList(cors.getAllowedOriginPatterns()));
         configuration.setAllowedOrigins(Arrays.asList(cors.getAllowedOrigins()));
         configuration.setAllowedMethods(Arrays.asList(cors.getAllowedMethods()));
         configuration.setMaxAge(cors.getMaxAge());
         configuration.setAllowedHeaders(Arrays.asList(cors.getAllowedHeaders()));
         configuration.setExposedHeaders(Arrays.asList(cors.getExposedHeaders()));
-
         configuration.setAllowCredentials(true);
         //configuration.setAllowPrivateNetwork(false);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
         source.registerCorsConfiguration("/**", configuration);
 
--- src/main/java/com/baeldung/resource/web/controller/FooController.java
+++ src/main/java/com/baeldung/resource/web/controller/FooController.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,7 +30,6 @@ public class FooController {
         this.fooService = fooService;
     }
 
-    //@CrossOrigin(origins = "http://localhost:8089")
     @GetMapping(value = "/{id}")
     public FooDto findOne(@PathVariable Long id) {
         Foo entity = fooService.findById(id)
