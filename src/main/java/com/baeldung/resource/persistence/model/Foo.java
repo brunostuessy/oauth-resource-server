95da0ea0a6703541a1ec2bc381130d1e472c535c
brunostuessy <bruno.stuessi@gmail.com>
2024-11-22 11:20:15 +0100

    Refactor

--- src/main/java/com/baeldung/resource/persistence/model/Foo.java
+++ src/main/java/com/baeldung/resource/persistence/model/Foo.java
@@ -5,6 +5,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+import java.util.Objects;
+
 @Entity
 public class Foo {
 
@@ -13,6 +15,7 @@ public class Foo {
     private Long id;
 
     private String name;
+
 
     protected Foo() {
     }
@@ -39,12 +42,7 @@ public class Foo {
 
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
+        return Objects.hash(id, name);
     }
 
     @Override
@@ -55,22 +53,14 @@ public class Foo {
             return false;
         if (getClass() != obj.getClass())
             return false;
-        Foo other = (Foo) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        return true;
+        final var other = (Foo) obj;
+        return Objects.equals(this.id, other.id)
+            && Objects.equals(this.name, other.name);
     }
 
     @Override
     public String toString() {
         return "Foo [id=" + id + ", name=" + name + "]";
     }
+
 }
